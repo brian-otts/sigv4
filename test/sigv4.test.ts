2c98286476774542e2b7ec667b1da96832c51bab
Brian Otts <brian.otts@cityelectricsupply.com>
2024-07-29 10:22:37 -0500

    Initial commit

---
+++ test/sigv4.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Sigv4 from '../lib/sigv4-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sigv4-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Sigv4.Sigv4Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
